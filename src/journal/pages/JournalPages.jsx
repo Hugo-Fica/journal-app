1f0250f76437fdf50799530ed8b066e8357f6705
Hugo Fica <hugo.fica.saez@gmail.com>
2022-10-09 20:51:04 -0300

    sidebar and navbar

--- src/journal/pages/JournalPages.jsx
+++ src/journal/pages/JournalPages.jsx
@@ -1,7 +1,13 @@
+import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 
 export const JournalPages = () => {
   return (
-    <h1>JournalPages</h1>
+    <JournalLayout>
+      <Typography>
+        Adipisicing quis cupidatat et in in ipsum adipisicing. Veniam laborum cupidatat sit occaecat sint id do nulla aliqua. Amet esse consectetur mollit ad velit Lorem et incididunt cupidatat incididunt aliqua. Nisi adipisicing laboris officia dolor officia reprehenderit reprehenderit. Irure anim officia deserunt deserunt ex nostrud tempor voluptate aliquip velit. Minim proident non ea nulla ex reprehenderit amet sunt irure proident magna.
+      </Typography>
+    </JournalLayout>
   )
 }
